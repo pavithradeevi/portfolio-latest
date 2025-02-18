7e1f138254b8775262250419993a3a01be826da6
Pavithra-0612 <pavithrafreelancer0612@gmail.com>
2025-02-18 20:51:07 +0100

    cv update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -45,7 +45,7 @@ const Home = () => {
         <div className="button-container">
   <button
     className="view-cv-button"
-    onClick={() => window.open("https://drive.google.com/file/d/1heiT3w02DGk-hRZUVTiLAc5v6pJFRhcF/view?usp=sharing", "_blank")}
+    onClick={() => window.open("https://drive.google.com/file/d/1AZliaNl8TcNb0M95sqt_GuI7U0VUGn0d/view?usp=sharing", "_blank")}
   >
     View CV
   </button>
